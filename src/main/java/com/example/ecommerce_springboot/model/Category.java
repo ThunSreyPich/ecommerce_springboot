0a57f9e35b49ac7e6a7945a87c4b6f918029f80c
T.Sreypich <sreypich.thun@student.passerellesnumeriques.org>
2025-07-23 23:39:25 +0700

    Edit category

--- src/main/java/com/example/ecommerce_springboot/model/Category.java
+++ src/main/java/com/example/ecommerce_springboot/model/Category.java
@@ -3,35 +3,19 @@ package com.example.ecommerce_springboot.model;
 import jakarta.persistence.*;
 import java.util.List;
 
-@Entity
 @Table(name = "categories")
+@Entity
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
     private String name;
-
-    // Optional: Show products in category
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
-    private List<Product> products;
-
-    public Category() {}
-
-    public Category(String name) {
-        this.name = name;
-    }
 
     // Getters and Setters
     public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     public String getName() { return name; }
-
     public void setName(String name) { this.name = name; }
-
-    public List<Product> getProducts() { return products; }
-
-    public void setProducts(List<Product> products) { this.products = products; }
 }
